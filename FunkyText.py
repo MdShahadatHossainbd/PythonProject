0189c94ecb43b03bf07fe2c51eaa29c135899e13
Md Shahadat Hossain <32963275+MdShahadatHossainbd@users.noreply.github.com>
2020-07-20 12:44:37 +0600

    Add files via upload

---
+++ FunkyText.py
@@ -0,0 +1,6 @@
+# pip install pyfiglet
+import pyfiglet
+word = pyfiglet.figlet_format("Shahadat")
+print(word)
+word = pyfiglet.figlet_format("Shahadat",font="alphabet")
+print(word)
---
+++ TerminalColorChange.py
@@ -0,0 +1,8 @@
+# pip install colorama
+
+from colorama import init
+init()
+from colorama import Fore , Back ,Style
+print(Fore.GREEN,"Hello, Bangladesh")
+print(Back.RED,"Md Shahadat Hossain")
+# print(Style.RESET_ALL)
